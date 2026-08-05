499e915754a814996a29a5e5db39b3b8fe528635
Marco Borrini <borrinimarco74@gmail.com>
2026-08-05 07:59:15 +0200

    rinutils: fix typo

--- src/rinutils.mk
+++ src/rinutils.mk
@@ -5,7 +5,7 @@ $(PKG)_WEBSITE  := https://github.com/shlomif/rinutils
 $(PKG)_DESCR    := Rinutils
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.10.3
-$(PKG)_CHECKSUM := f9e527d37a6cc8c7b8870ada63caa24f32ab0d29fd1116df3ebb686583030955 
+$(PKG)_CHECKSUM := f9e527d37a6cc8c7b8870ada63caa24f32ab0d29fd1116df3ebb686583030955
 $(PKG)_GH_CONF  := shlomif/rinutils/releases,,,,,.tar.xz
 $(PKG)_DEPS     := cc
 
